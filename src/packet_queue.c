3836d150ccfa4c739c921675a908ad8972d6d79c
Alonso Veliz Garcia <alonsovelizgarcia12@gmail.com>
2025-03-24 22:46:31 +0100

    Added mutex conditions of queue

--- src/packet_queue.c
+++ src/packet_queue.c
@@ -16,6 +16,18 @@ bool init_packet_queue(void){
     fprintf(stderr, "Failed to initialize queue mutex\n");
     return false;
   }
+
+  if((pthread_cond_init(&packet_queue.not_empty, NULL)) != 0){
+    fprintf(stderr, "Failed to initialize not empty queue cond");
+    pthread_mutex_destroy(&packet_queue.mutex);
+    return false;
+  }
+
+  if((pthread_cond_init(&packet_queue.not_full, NULL)) != 0){
+    fprintf(stderr, "Failed to initialize not_full condition\n");
+    pthread_cond_destroy(&packet_queue.not_empty);
+    pthread_mutex_destroy(&packet_queue.mutex);
+  }
   return true;
 }
 
@@ -35,8 +47,14 @@ bool enqueue_packet(const u_char* pkt_data, size_t len, struct timeval timestamp
   memcpy(packet_queue.packets[packet_queue.head].data, pkt_data, len);
   packet_queue.packets[packet_queue.head].len = len;
   packet_queue.packets[packet_queue.head].timestamp = timestamp;
-  // THINGS TO DO
+  
+  packet_queue.head = (packet_queue.head + 1);
+  packet_queue.count++;
+
+  // We tell the flow manager that there are packets to process
+  pthread_cond_signal(&packet_queue.not_empty);
   pthread_mutex_unlock(&packet_queue.mutex);
+
   return true;
 }
 
