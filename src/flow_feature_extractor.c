6c0a50ab2a4ccd67f0a72c1020cafabb45c31151
Alonso Veliz Garcia <alonsovelizgarcia12@gmail.com>
2025-03-25 11:45:58 +0100

    Added thread init (thread_func to implement)

--- src/flow_feature_extractor.c
+++ src/flow_feature_extractor.c
@@ -3,6 +3,8 @@
 #include "nids_backend.h"
 #include <stdbool.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
 
 typedef struct flow_entry{
   flow_stats_t stats;
@@ -12,6 +14,9 @@ typedef struct flow_entry{
 struct flow_entry_t** flow_table = NULL;
 static int flow_count = 0;
 static volatile bool running = false;
+
+void* flow_manager_thread_func(void* arg);
+pthread_t flow_manager_thread;
 
 bool initialize_feature_extractor(nids_config_t* session_config){
   config = session_config;
@@ -31,7 +36,35 @@ bool initialize_feature_extractor(nids_config_t* session_config){
   return true; 
 }
 
+bool start_flow_manager(void){
+  if(running){
+    fprintf(stderr, "Flow manager is already running!\n");
+    return false;
+  }
+
+  if(pthread_create(&flow_manager_thread, NULL, flow_manager_thread_func, NULL) != 0){
+    fprintf(stderr, "Flow manager could not be created properly\n");
+    running = false;
+    return false;
+  }
+  running = true;
+  return true;
+}
 
 int get_flow_hash(const u_char* pkt_data){
   return 1;
+}
+
+void* flow_manager_thread_func(void* arg){
+  printf("THREAD_FEATURE_EXTRACTOR: Inside flow_manager_thread_func\n");
+
+  if(pthread_detach(pthread_self()) != 0){
+    fprintf(stderr, "THREAD_FEATURE_EXTRACOTR: Wasnt detached successfully\n");
+  }
+  pthread_setname_np(pthread_self(), "flow_mngr");
+  
+  while(running){
+    sleep(1);
+  }
+  return NULL;
 }
