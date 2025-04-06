7357c910b884cc707d6a3ed01bb8f209459c07c4
Alonso Veliz Garcia <alonsovelizgarcia12@gmail.com>
2025-04-06 16:54:57 +0200

    Get flow key func

--- src/flow_feature_extractor.c
+++ src/flow_feature_extractor.c
@@ -5,7 +5,11 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
-
+#include <arpa/inet.h>
+#include <net/ethernet.h>
+#include <string.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
 typedef struct flow_entry{
   flow_stats_t stats;
   struct flow_entry* next;
@@ -51,8 +55,35 @@ bool start_flow_manager(void){
   return true;
 }
 
-int get_flow_hash(const u_char* pkt_data){
-  return 1;
+flow_key_t get_flow_key(const u_char* pkt_data){
+  flow_key_t flow_key;
+  memset(&flow_key, 0, sizeof(flow_key));
+  
+  struct ether_header* eth_header = (struct ether_header*)pkt_data;
+  u_char* ip_packet = pkt_data + sizeof(struct ether_header);
+  
+  struct ip* ip_header = (struct ip*)ip_packet;
+
+  flow_key.src_ip = ntohl(ip_header -> ip_src.s_addr);
+  flow_key.dst_ip = ntohl(ip_header->ip_dst.s_addr);
+  flow_key.protocol = ip_header->ip_p;
+
+  unsigned int ihl = ip_header->ip_hl;
+
+  u_char* tcp_packet = ip_packet + ihl * 4;
+  if(flow_key.protocol == IPPROTO_TCP){
+    struct tcphdr* tcp_header = (struct tcphdr*) tcp_packet;
+    flow_key.src_port = tcp_header->th_sport;
+    flow_key.dst_port = tcp_header->th_dport;
+  }
+  printf("[get_flow_key] SRC_IP: %u \n", flow_key.src_ip);
+  printf("[get_flow_key] DST_IP: %u \n", flow_key.dst_ip);
+  printf("[get_flow_key] PROTOCOL: %u \n", flow_key.protocol);
+  printf("[get_flow_key] SRC_PORT: %u \n", flow_key.src_port);
+  printf("[get_flow_key] DST_PORT: %u \n", flow_key.dst_port);
+
+
+   return flow_key;
 }
 
 void stop_flow_manager(void){
@@ -88,5 +119,8 @@ void* flow_manager_thread_func(void* arg){
 }
 
 void process_packet(u_int8_t* data, size_t len){
-  printf("Protocol %u \n", data[8]);
+  flow_key_t key = get_flow_key(data);
+  
 }
+
+
