2f558e2e0d2955cd4b44a248f1543db626f3fe31
Alonso Veliz Garcia <alonsovelizgarcia12@gmail.com>
2025-05-02 11:20:20 +0200

    Added more features

--- include/nids_backend.h
+++ include/nids_backend.h
@@ -45,36 +45,43 @@ typedef struct{
     flow_key_t key;
 
     bool expired;
-    flow_status_t status;            // Flow [active, idle]
-    uint64_t idle_time;              // Time in which i'm not receiving any packets
+    flow_status_t status;            // Flow [active, idle, closed, expired]
+    uint64_t last_checked_time;      // Last time that the flow was updated or received a packet 
     flow_close_state_t close_state; 
+    //
 
     uint32_t dst_ip_fwd;             // Feature to check if flow is fwd or bwd
     uint32_t flow_hash;              // Hash of the flow key
-    
+    //
+
     // Flow duration
     uint64_t flow_start_time;        // Start timestamp of the flow
-    uint64_t flow_last_time;         // Last seen timestamp
+    uint64_t flow_last_time;         // Last seen timestamp (last packet seen in flow)
     uint64_t flow_duration;          // Duration of the flow in microseconds
-    
+    //
+
     // Packet counts
     uint32_t total_fwd_packets;      // Total packets in forward direction
     uint32_t total_bwd_packets;      // Total packets in backward direction
-    
+    //
+
     // Size-based features
     uint64_t total_fwd_bytes;        // Total bytes in forward direction
     uint64_t total_bwd_bytes;        // Total bytes in backward direction
-   
+    //
+
     uint16_t fwd_packet_len_min;     // Min packet size in forward direction
     uint16_t fwd_packet_len_max;     // Max packet size in forward direction
     double   fwd_packet_len_mean;    // Mean packet size in forward direction
     double   fwd_packet_len_std;     // Std dev of packet size in forward direction
-    
+    //
+
     uint16_t bwd_packet_len_min;     // Min packet size in backward direction
     uint16_t bwd_packet_len_max;     // Max packet size in backward direction
     double   bwd_packet_len_mean;    // Mean packet size in backward direction
     double   bwd_packet_len_std;     // Std dev of packet size in backward direction
-    
+    //
+
     // Flow rate features
     double   flow_bytes_per_sec;     // Flow bytes per second
     double   flow_packets_per_sec;   // Flow packets per second
@@ -85,29 +92,34 @@ typedef struct{
     uint64_t flow_iat_max;           // Max time between packets
     uint64_t flow_iat_min;           // Min time between packets
     uint64_t flow_iat_total;         // For then to compute with packet num
+    //
 
     uint64_t fwd_iat_min;            // Min time between forward packets
     uint64_t fwd_iat_max;            // Max time between forward packets
     double   fwd_iat_mean;           // Mean time between forward packets
     double   fwd_iat_std;            // Std dev of time between forward packets
     uint64_t fwd_iat_total;          // Total time between forward packets
-    
+    //
+
     uint64_t bwd_iat_min;            // Min time between backward packets
     uint64_t bwd_iat_max;            // Max time between backward packets
     double   bwd_iat_mean;           // Mean time between backward packets
     double   bwd_iat_std;            // Std dev of time between backward packets
     uint64_t bwd_iat_total;          // Total time between backward packets
-    
+    //
+
     // FWD && BWD Specific flag counts
     uint16_t fwd_psh_flags;          // Number of PSH flags in forward direction
     uint16_t bwd_psh_flags;          // Number of PSH flags in backward direction
     uint16_t fwd_urg_flags;          // Number of URG flags in forward direction
     uint16_t bwd_urg_flags;          // Number of URG flags in backward direction
+    //
     
     // Header information
     uint32_t fwd_header_len;         // Total bytes used for headers in forward direction
     uint32_t bwd_header_len;         // Total bytes used for headers in backward direction
-    
+    //
+
     // Packet rate
     double   fwd_packets_per_sec;    // Forward packets per second
     double   bwd_packets_per_sec;    // Backward packets per second
@@ -118,7 +130,8 @@ typedef struct{
     double   packet_len_mean;        // Mean length of a packet
     double   packet_len_std;         // Std dev of packet length
     double   packet_len_variance;    // Variance of packet length
-    
+    //
+
     // Flag counts
     uint16_t fin_flag_count;         // Number of packets with FIN
     uint16_t syn_flag_count;         // Number of packets with SYN
@@ -128,7 +141,8 @@ typedef struct{
     uint16_t urg_flag_count;         // Number of packets with URG
     uint16_t cwr_flag_count;         // Number of packets with CWR
     uint16_t ece_flag_count;         // Number of packets with ECE
-    
+    //
+
     // Ratio and averages
     double down_up_ratio;            // Download and upload ratio
     double avg_packet_size;          // Average size of packet
@@ -138,8 +152,10 @@ typedef struct{
     double bwd_segment_size_tot;     // Sum of size in bwd direction
     double fwd_seg_size_min;         // Minimum segment size in forward direction
 
-    // Bulk features
-    bool last_fwd_packet_is_bulk;      
+
+    // FWD Bulk Features
+    bool in_fwd_bulk_transmission;
+    int count_possible_fwd_bulk_packets;
     int num_fwd_bulk_transmissions;  // Num of fwd bulk transmissions
     time_t fwd_bulk_start;
     time_t fwd_bulk_end;
@@ -150,8 +166,10 @@ typedef struct{
     double fwd_bytes_bulk_avg;     // Average bytes bulk rate in forward direction
     double fwd_packet_bulk_avg;    // Average packet bulk rate in forward direction
     double fwd_bulk_rate_avg;      // Average bulk rate in forward direction
-    
-    bool last_bwd_packet_is_bulk;
+   
+    // BWD Bulk features
+    bool in_bwd_bulk_transmission;
+    int count_possible_bwd_bulk_packets;
     int num_bwd_bulk_transmissions;
     time_t bwd_bulk_start;
     time_t bwd_bulk_end;
@@ -183,13 +201,21 @@ typedef struct{
     double   active_mean;            // Mean time flow was active before becoming idle
     uint64_t active_max;             // Maximum time flow was active before becoming idle
     double   active_std;             // Std dev of time flow was active before becoming idle
-    
-    uint64_t idle_time_tot;
-    uint64_t curr_idle_time_tot;
+    //
+
+
+    uint64_t idle_time_tot;          // Current active time, if turns idle:0
+    uint64_t curr_idle_time_tot;     // Current idle time, if turns active: 0
     uint64_t idle_min;               // Minimum time flow was idle before becoming active
     double   idle_mean;              // Mean time flow was idle before becoming active
     uint64_t idle_max;               // Maximum time flow was idle before becoming active
-    double   idle_std;               // Std dev of time flow was idle before becoming active 
+    double   idle_std;               // Std dev of time flow was idle before becoming active
+    //
+
+    bool classified;
+    bool benign;
+    float confidence;
+
 } flow_stats_t;
 
 typedef struct {
